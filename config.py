0a894218aa3420e4a0d2a697e549207466cdacee
ilavir <1428978+ilavir@users.noreply.github.com>
2024-07-14 16:56:03 +0300

    added "SESSION_COOKIE_NAME" to Config to avoid browser sessions conflict

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'fb731c467c3df36554f09c6635bc4d8e619d1f96cc3778bbcfa3487dc435a21a'
+    SESSION_COOKIE_NAME = os.environ.get('SESSION_COOKIE_NAME') or 'zcf34765_session'
     CLOUDFLARE_API_BASE_URL = 'https://api.cloudflare.com/client/v4/'
